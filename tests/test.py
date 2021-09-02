0086f4cd87ff622cc0216079f3a6ee758d4eb433
Daniel Diaz <daniel.diaz@ucl.ac.uk>
2021-09-02 13:25:51 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,25 @@
-# import csv
-# import stimlist as stl
+import csv
+import stimlist as stl
+
+f = open("test.csv", "r")
+cc = csv.reader(f)
+next(cc)
+in_seq = []
+for row in cc:
+    in_seq.append(row)
+
+my_list = stl.StimList(in_seq, 2, k=3)
+
+my_list = my_list.prand_seq()
+
+# mynewlist = [y for x,y in sorted(zip(mylist,in_seq))]
 #
-# f = open("test.csv","r")
-# cc = csv.reader(f)
-# next(cc)
-# in_seq = []
-# for row in cc: in_seq.append(row)
-
-#mylist = stl.StimList(in_seq,2,k=3)
-
-#mylist = mylist.prand_seq()
-
-#mynewlist = [y for x,y in sorted(zip(mylist,in_seq))]
-
-inp = [['1','blue','road','motorbike'],['2','red','air','helicopter'],['3','green','sea','ship'],['4','white','air','airplane'],['5','black','road','car']]
-outp = [['5','car'],['4','airplane'],['3','ship'],['2','helicopter'],['1','motorbike']]
-
-mynewlist = [idx for idx,trial in enumerate(outp) if '4' in trial]
+# inp = [['1','blue','road','motorbike'],['2','red','air','helicopter'],['3','green','sea','ship'],['4','white','air','airplane'],['5','black','road','car']]
+# outp = [['5','car'],['4','airplane'],['3','ship'],['2','helicopter'],['1','motorbike']]
+#
+# mynewlist = [idx for idx,trial in enumerate(outp) if '4' in trial]
 
 
-# print(mylist)
+print(my_list)
 # print(' ')
-print(mynewlist)
+# print(mynewlist)
