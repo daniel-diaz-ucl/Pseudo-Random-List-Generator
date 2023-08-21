cc4fc5f93b6308743a5707052fe086a915622c11
Daniel Diaz <46817561+daniel-diaz-ucl@users.noreply.github.com>
2023-08-21 11:55:57 +0100

    delete-idea-ide-folder

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,16 @@ author = 'Daniel Diaz'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.coverage', 
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary']
+
+# Set default options for the automodule and autoclass directives
+autodoc_default_options = {
+    'members': True,
+    'private-members': True,
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,4 +58,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
